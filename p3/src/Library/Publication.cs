4668ba7a4e139fb77768e3d1119421f1173e5e62
N4choCorrea <correa.ignacio48@gmail.com>
2022-12-23 08:51:46 -0300

    ejercicio resuelto

--- p3/src/Library/Publication.cs
+++ p3/src/Library/Publication.cs
@@ -33,5 +33,17 @@ namespace Ucu.Poo.Defense
         {
             this.items.Remove(item);
         }
+        //Aqui aplicamos el patron expert porque Publication es el experto en informacion por lo que
+        //es el que deberia tener la responsabilidad de calcular el total
+        public double Total()
+        {
+            int total = 0;
+            foreach (PublicationItem item in this.items)
+            {
+                total += item.Quantity * item.Price;
+                
+            }
+            return total;
+        }
     }
 }
